8e03ecd5e7b0d3baf9ae8db1de455812325095c9
deryardi73 <deryardi73@gmail.com>
2026-07-30 13:54:11 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,9 +1,9 @@
 #CONFIGURATION
-kernelsource=https://github.com/xxblebleblexx/moonbeam_gale_kernel.git # Must be edited
+kernelsource=https://github.com/deryardi73/android_kernel_xiaomi_fire.git # Must be edited
 kernelname=$(basename "$kernelsource" .git) # No need to edit
-branch_kernel=moonbeam # Must be edited
-defconfig_path=arch/arm64/configs/gale_defconfig # Must be edited
-defconfig=gale_defconfig # Must be edited
+branch_kernel=inferno2 # Must be edited
+defconfig_path=arch/arm64/configs/fire_defconfig # Must be edited
+defconfig=fire_defconfig # Must be edited
 fast_path=$GITHUB_WORKSPACE # This where kernelsource saved
 hooks=manual #only manual hook/kprobes hook, must be edited
 
@@ -12,7 +12,7 @@ git clone -b $branch_kernel --depth=1 $kernelsource;wait
 cd $fast_path/$kernelname
 
 #KSU DRIVER
-curl -LSs "https://raw.githubusercontent.com/xxblebleblexx/MultiSU/refs/heads/legacy/kernel/setup.sh" | bash -s legacy
+curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -s stable
 
 #KSU ACTIVATION
 echo "CONFIG_KSU=y" >> $defconfig_path
@@ -27,7 +27,7 @@ fi
 if [ "$hooks" = "manual" ]; then
 #MANUAL HOOK
 echo "CONFIG_KSU_MANUAL_HOOK=y" >> $defconfig_path
-wget https://raw.githubusercontent.com/xxblebleblexx/manual_hook_fix/refs/heads/main/manualhook_1.6_fixed.patch;wait;patch -p1 < manualhook_1.6_fixed.patch
+wget https://raw.githubusercontent.com/deryardi73/manual_hook/refs/heads/main/manualhook_1.6_fixed.patch;wait;patch -p1 < manualhook_1.6_fixed.patch
 fi
 
 make O=out ARCH=arm64 $defconfig; printf "Y\n2\n\n\n\nY\n" | make -j$(nproc --all) CC=clang O=out ARCH=arm64 LLVM=1 LLVM_IAS=1 LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump READELF=llvm-readelf STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu-
