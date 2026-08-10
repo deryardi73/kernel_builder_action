422109b31c188e057fdf8f279e477a9b9ba51580
deryardi73 <deryardi73@gmail.com>
2026-08-10 17:45:02 +0700

    Update kernel.sh for SUSFS configuration changes
    Removed unnecessary configuration options and added new ones for SUSFS.

--- kernel.sh
+++ kernel.sh
@@ -32,16 +32,14 @@ wget https://raw.githubusercontent.com/deryardi73/manual_hook/refs/heads/main/ma
 fi
 
 if [ "$susfs" = "y" ]; then
+curl -LSs https://raw.githubusercontent.com/Youffx/KernelSU-Next/legacy-susfs/kernel/setup.sh | bash -s legacy-susfs
 echo "CONFIG_KSU_SUSFS=y" >> $defconfig_path
-echo "CONFIG_KSU_SUSFS_SUS_PATH=y" >> $defconfig_path
 echo "CONFIG_KSU_SUSFS_SUS_MOUNT=y" >> $defconfig_path
 echo "CONFIG_KSU_SUSFS_SUS_KSTAT=y" >> $defconfig_path
-echo "CONFIG_KSU_SUSFS_SUS_MAP=y" >> $defconfig_path
-echo "CONFIG_KSU_SUSFS_OPEN_REDIRECT=y" >> $defconfig_path
 echo "CONFIG_KSU_SUSFS_SPOOF_UNAME=y" >> $defconfig_path
-echo "CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG=y" >> $defconfig_path
-echo "CONFIG_KSU_SUSFS_ENABLE_LOG=y" >> $defconfig_path
-wget https://raw.githubusercontent.com/deryardi73/NonGKI_Kernel_Build_2nd/refs/heads/mainline/Patches/Patch/susfs_patch_to_4.19.patch;wait;patch -p1 < susfs_patch_to_4.19.patch
+echo "CONFIG_KSU_SUSFS_HIDE_KSU_SUSFS_SYMBOLS=y" >> $defconfig_path
+echo "CONFIG_KSU_SUSFS_OPEN_REDIRECT=y" >> $defconfig_path
+echo "CONFIG_KSU_SUSFS_SUS_MAP=y" >> $defconfig_path
 fi
 
 make O=out ARCH=arm64 $defconfig; printf "Y\n2\n\n\n\nY\n" | make -j$(nproc --all) CC=clang O=out ARCH=arm64 LLVM=1 LLVM_IAS=1 LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump READELF=llvm-readelf STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu-
