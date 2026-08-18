8e75934089085b8d173981cb093f2b4a2c0dec51
deryardi73 <deryardi73@gmail.com>
2026-08-18 23:23:41 +0700

    Change branch name and remove KSU setup
    Updated the branch name for kernel source and removed KSU driver and activation steps.

--- kernel.sh
+++ kernel.sh
@@ -1,7 +1,7 @@
 #CONFIGURATION
 kernelsource=https://github.com/deryardi73/android_kernel_xiaomi_fire.git # Must be edited
 kernelname=$(basename "$kernelsource" .git) # No need to edit
-branch_kernel=inferno2 # Must be edited
+branch_kernel=inferno_mglru # Must be edited
 defconfig_path=arch/arm64/configs/fire_defconfig # Must be edited
 defconfig=fire_defconfig # Must be edited
 fast_path=$GITHUB_WORKSPACE # This where kernelsource saved
@@ -9,12 +9,5 @@ fast_path=$GITHUB_WORKSPACE # This where kernelsource saved
 cd $fast_path
 git clone -b $branch_kernel --depth=1 $kernelsource;wait
 cd $fast_path/$kernelname
-
-#KSU DRIVER
-curl -LSs "https://raw.githubusercontent.com/backslashxx/KernelSU/refs/heads/master/kernel/setup.sh" | bash -s master
-
-#KSU ACTIVATION
-echo "CONFIG_KSU=y" >> $defconfig_path
-echo "CONFIG_KSU_TAMPER_SYSCALL_TABLE=y" >> $defconfig_path
 
 make O=out ARCH=arm64 $defconfig; printf "Y\n2\n\n\n\nY\n" | make -j$(nproc --all) CC=clang O=out ARCH=arm64 LLVM=1 LLVM_IAS=1 LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump READELF=llvm-readelf STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu-
