589cc98bb36491af8dae67aa651020297581c056
deryardi73 <deryardi73@gmail.com>
2026-08-30 07:26:14 +0700

    Change kernel setup script and config options
    Updated kernel setup script source and configuration options.

--- kernel.sh
+++ kernel.sh
@@ -11,10 +11,11 @@ git clone -b $branch_kernel --depth=1 $kernelsource;wait
 cd $fast_path/$kernelname
 
 #KSU DRIVER
-curl -LSs "https://raw.githubusercontent.com/backslashxx/KernelSU/refs/heads/master/kernel/setup.sh" | bash -s master
+curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup.sh" | bash
+wget https://raw.githubusercontent.com/deryardi73/manual_hook/refs/heads/main/manualhook.patch;wait;patch -p1 < manualhook.patch
 
 #KSU ACTIVATION
 echo "CONFIG_KSU=y" >> $defconfig_path
-echo "CONFIG_KSU_TAMPER_SYSCALL_TABLE=y" >> $defconfig_path
+echo "CONFIG_KSU_MANUAL_HOOK=y" >> $defconfig_path
 
 make O=out ARCH=arm64 $defconfig; printf "Y\n2\n\n\n\nY\n" | make -j$(nproc --all) CC=clang O=out ARCH=arm64 LLVM=1 LLVM_IAS=1 LD=ld.lld AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump READELF=llvm-readelf STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu-
