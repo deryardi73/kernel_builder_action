97b1166efb76c9290ec28da6a7f30c4e4a04d8db
deryardi73 <deryardi73@gmail.com>
2026-09-10 18:21:45 +0700

    Update kernel repository and branch information

--- kernel.sh
+++ kernel.sh
@@ -9,8 +9,8 @@ helper=${branch_kernel#*-} # No need to edit
 compile_type=${helper%%-*} # No need to edit
  #USE OWN SOURCE KERNEL
 use_own_kernel=y # y/n 
-link_ur_kernel=https://github.com/deryardi73/gki_kernel.git #Must be edited
-branch_ur_kernel=6.6-lts #Must be edited
+link_ur_kernel=https://github.com/deryardi73/kernel_common.git #Must be edited
+branch_ur_kernel=6.6.30 #Must be edited
 #ksu option
 use_ksu=y
 
@@ -59,7 +59,7 @@ cd $fast_path
 
 case "$compile_type" in
     android13|android14|android15|android16)
-        ./tools/bazel build --config=fast --config=stamp --nokmi_symbol_list_strict_mode //common:kernel_aarch64_dist
+        ./tools/bazel build --config=fast --nokmi_symbol_list_strict_mode //common:kernel_aarch64_dist
         ;;
     android12)
         LTO=thin BUILD_CONFIG=common/build.config.gki.aarch64 build/build.sh
